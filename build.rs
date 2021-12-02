3595dab47753d5628928cce060e3015be846195b
CousinZe <2571851049@qq.com>
2021-12-02 15:29:37 +0800

    ~

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use std::env::current_dir;
+use std::path::PathBuf;
+
 fn main() {
+    let source_path: PathBuf = current_dir()
+        .unwrap()
+        .join("pl2")
+        .join("pl2b.c");
     cc::Build::new()
-        .file("./pl2/pl2b.c")
+        .file(source_path)
         .compile("pl2");
 }
