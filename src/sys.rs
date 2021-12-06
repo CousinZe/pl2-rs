a2c28e42d96587bda79c6cc936a18826217f92f5
CousinZe <2571851049@qq.com>
2021-12-06 15:25:16 +0800

    ~

--- src/sys.rs
+++ src/sys.rs
@@ -6,7 +6,7 @@ pub use crate::sys_types::*;
 extern "C" {
     pub(crate) fn pl2b_getLocaleName() -> *const c_char;
     pub(crate) fn pl2b_errorBuffer(buffer_size: u16) -> *mut Error;
-    pub(crate) fn pl2b_errPrintf(
+    pub fn pl2b_errPrintf(
         error: *mut Error,
         error_code: u16,
         source_info: SourceInfo,
@@ -17,7 +17,6 @@ extern "C" {
     pub(crate) fn pl2b_dropError(error: *mut Error);
     pub(crate) fn pl2b_isError(error: *mut Error) -> bool;
     pub(crate) fn pl2b_argsLen(cmd: *const Command) -> u16;
-    pub(crate) fn pl2b_initProgram(program: *mut Program);
     pub(crate) fn pl2b_parse(
         source: *mut c_char,
         parse_bufsiz: u16,
@@ -25,7 +24,6 @@ extern "C" {
     ) -> Program;
     pub(crate) fn pl2b_dropProgram(program: *mut Program);
     pub(crate) fn pl2b_debugPrintProgram(program: *const Program);
-    pub(crate) fn pl2b_parseSemVer(src: *const c_char, err: *mut Error);
     pub(crate) fn pl2b_run(program: *const Program, err: *mut Error);
 }
 
